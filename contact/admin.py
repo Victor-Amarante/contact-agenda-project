b850741abb170db236f14f3ebf9245bf6c5da26e
Victor Amarante <vafstats@gmail.com>
2024-10-23 12:23:01 -0300

    chore: customizing the options of admin.ModelAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,9 @@ from contact.models import Contact
 # Register your models here.
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-  ...
+  list_display = ('id', 'first_name', 'last_name', 'phone',)
+  ordering = '-id',
+  # list_filter = ('created_date',)
+  search_fields = ('id', 'first_name',)
+  list_per_page = 10
+  list_max_show_all = 200
